17f5da5d113e01e6de0f72411ac2fb05cf793bfd
serg <seregey.balandin@gmail.com>
2020-11-29 11:38:37 +0200

    refactor file utilitys

--- src/utilitys/utilitys.js
+++ src/utilitys/utilitys.js
@@ -4,6 +4,7 @@ import {LoginManager} from 'react-native-fbsdk';
 import {GoogleSignin} from '@react-native-community/google-signin';
 import axios from 'axios';
 
+//initail API instance , connect to database firebase .
 const instance = axios.create({
   baseURL: APIS.firebasedb,
   timeout: 1500,
@@ -11,6 +12,8 @@ const instance = axios.create({
     'Content-Type': 'application/json',
   },
 });
+
+//get info on wich OS the app is working 
 export const OS = Platform.OS === 'ios' ? 'ios' : 'android';
 
 export const NOOP = () => {};
@@ -19,13 +22,13 @@ export const signout = async (typeSingout, cb) => {
   switch (typeSingout) {
     case signinWith.facebook:
       LoginManager.logOut();
-      cb();
+      cb && cb(); // if cb is not undefind then exe the func 
       break;
     case signinWith.google:
       try {
         await GoogleSignin.revokeAccess();
         await GoogleSignin.signOut();
-        cb();
+        cb && cb();  // if cb is not undefind then exe the func 
       } catch (error) {
         console.error(error);
       }
@@ -34,17 +37,17 @@ export const signout = async (typeSingout, cb) => {
       return;
   }
 };
-
+// Handle to method GET - for fecth data from data base firebase
 export const getHandle = async () => new Promise(async (resolve) => {
   const response = await instance.get('/holon.json')
   resolve(response.data)
 });
 
-
+// Handle to method PUT - for update data from data base firebase
 export const putHandle = (path, body) => new Promise(async (resolve) => {
     const response = await instance.put(path, body)
     resolve(response.data)
   
 })
-
+// find index of item in arr - Generic Function. 
 export const findIndex = (arr, propety, value) => arr.findIndex(item => item[propety] === value)
